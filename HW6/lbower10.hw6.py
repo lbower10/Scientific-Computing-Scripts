d9f80b56faace353ac1c43434605cf76089cef77
lbower10 <lbower10@vols.utk.edu>
2023-07-30 07:25:40 -0400

    hw6 might be complete??

--- HW6/lbower10.hw6.py
+++ HW6/lbower10.hw6.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-#
-# Template for Problem 10 from Problem Set 3.2
-#
-# Insert a comment block describing the problem and curve fitting
-# approach deployed.  Also, be sure your name and assignment
-# number are included in your comments.
-#
-# ???
-# ???
-# ???
-# ???
-# ???
-# ???
+'''
+    File: lbower10.hw6.py
+    Author: Logan Bowers
+    COSC 360 HW6
+    Curve fitting using polynomial interpolation
+'''
 
 from numpy import array,zeros
 from polyFit import *
-import pylab
+import matplotlib.pyplot as pylab
 
 def evalPoly(c,x): # c stores coefficients for polynomial
     m = len(c) - 1 # (copied from polyFit module)
@@ -24,8 +17,8 @@ def evalPoly(c,x): # c stores coefficients for polynomial
         p = p*x + c[m-j-1]
     return p
 
-xData = array(???) // xData array contains the years
-yData = array(???) // yData array contains the efficiencies (as percentages)
+xData = array([1718, 1767, 1774, 1775, 1792, 1816, 1828, 1834, 1878, 1906]) # xData array contains the years
+yData = array([0.5, 0.8, 1.4, 2.7, 4.5, 7.5, 12.0, 17.0, 17.2, 23.0]) # yData array contains the efficiencies (as percentages)
 
 minsdev=float("inf")
 minpoly=0
@@ -34,29 +27,27 @@ print('Degree  Stdev   2000P')
 for m in range(1,6):   # Try m=1,2,3,4,5 (degree of polynomial)
     ys=zeros((n),dtype='float') # initialize y-coordinates for polynomial curve
 
-    coeff = ??? # get coefficients for n-th degree polynomial
-    stdev = ??? # get stdev of the error in the fit
-    proj  = ??? # evaluate the polynomial at year 2000
-#
+    coeff = polyFit(xData, yData, m) # get coefficients for n-th degree polynomial
+    stdev = stdDev(coeff, xData, yData) # get stdev of the error in the fit
+    proj  = evalPoly(coeff, 2000) # evaluate the polynomial at year 2000
+
 #   Year 2000 projections >= 100 or < 0 are meaniningless        
-#
     if (stdev < minsdev) and proj < 100 and proj > 0 :
 
         print('{:3d}\t{:5.3f}\t{:5.3f}\t{:s}'.format(m,stdev,proj,'viable'))
-        ys= ??? # get y-coordinates of polynomial using x-coordinates in xData array
-#
+        ys= [evalPoly(coeff, x) for x in xData] # get y-coordinates of polynomial using x-coordinates in xData array
+
 #       Use Matplotlib to plot original data points with validated curve fitting
-#
         pylab.figure()
         pylab.xlabel("x")
         pylab.ylabel("Thermal Efficiency")
         my_title= 'Fit with poly degree = ' + str(m) + '; green dot is 2000 projection'
         pylab.title(my_title)
-        pylab.xlim(???)            # x-axis values should range from 1710 to 2015
-        pylab.plot(???)            # plot Year 2000 projection as a green dot
-        pylab.plot(???)            # plot the original data given in the xData and yData
+        pylab.xlim(1710, 2015)            # x-axis values should range from 1710 to 2015
+        pylab.plot(2000, proj, 'go')            # plot Year 2000 projection as a green dot
+        pylab.plot(xData, yData, 'ro')            # plot the original data given in the xData and yData
                                    # arrays as red dots
-        pylab.plot(???)            # plot polynomial curve using xData and ys arrays
+        pylab.plot(xData, ys, 'b-')            # plot polynomial curve using xData and ys arrays
                                    # and make it blue.
         pylab.grid()
         fname='degree' + str(m) + 'fit.png' # save figure to file rather than display
