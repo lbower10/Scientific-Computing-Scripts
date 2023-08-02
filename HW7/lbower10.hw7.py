2a461ae83de322684899986caf072fc77054c823
lbower10 <lbower10@vols.utk.edu>
2023-08-02 17:33:10 -0500

    hw7 done

--- HW7/lbower10.hw7.py
+++ HW7/lbower10.hw7.py
@@ -1,61 +1,32 @@
 #!/usr/bin/env python3
-#
-# Template for Problem 27 from Problem Set 4.1
-# You must complete/finish all statements with "???"
-#
+'''
+    Logan Bowers --- lbower10
+    COSC 370 HW7
+    R - Radial distance from center of Earth to satellite
+    Theta - Satellite's angle at its location in orbit
+    C - Constant in equation
+    e - Eccentricity of orbit, descripes orbital shape
+    Alpha - Phase angle/angular shift of orbit
+ '''
+
 from newtonRaphson2 import *
 from math import sin, pi
 import numpy as np
 import matplotlib.pyplot as plt
 
-# The trajectory of a satellite orbiting the earth is given by the equation
-#
-# R = C/(1+e*sin(theta + alpha)), which can also be written as
-# R - C/(1+e*sin(theta + alpha)) = 0 (in order to exploit rootfinding methods).
-#
-#  In this comment block, define the following parameters:  R, theta, C,
-#  e, and alpha.  Provide your name and assignment number in this comment
-#  block also.
-#
-#  Logan Bowers --- lbower10
-#  COSC 370 HW7
-#  R - Distance from center of Earth to satellite
-#  Theta - Satellite's angular position in orbit
-#  C - Constant, semi-major axis of satellite's orbit
-#  e - Eccentricity of orbit, descripes orbital shape
-#  Alpha - Phase angle of orbit
-#
-#  Given (R, theta) data pairs: (6870km, -30deg),(6728km, 0deg), (6615km, 30deg)
-#
-#  The Newton-Raphson method can be used to solve a nonlinear system of 
-#  3 trajectory equations using the given data. The unknowns are stored in
-#  the vector x = [ C, e, alpha], and the initial guess is [6800, 0.5, 0].
-#
-#   In solving the resulting nonlinear system for x, the derived constants 
-#   can be used to determine the minimum trajectory and angle at which it
-#   occurs. The trajectory equation of the satellite's orbit as well as
-#   the point at which minimum trajectory occurs can be plotted.
-#
-# Create the vector-valued function F(x) whose root defines a system of 3 trajectory 
-# equations (using given data); use radians for all angles.
-#
-# Let x[0]=C, x[1]=e, and x[2]=alpha, and 30 degrees be represented by pi/6.
-# Then, we seek a vector x that solves R- x[0]]/(1+x[1]*sin(theta + x[2])) = 0, [Eqn1]
-# for each data pair given above.
-#
-
 #  Complete the following function below, where F[j] returns the left-hand-side
 #  of [Eqn1] where R and theta are given by the jth data pair.
 def F(x):
-    C, e, alpha = x[0], x[1], x[2]
+    # from original comments (I removed them because they drove me crazy)
+    R, theta = np.array([6870, 6728, 6615]), np.array([-30, 0, 30]) * (pi / 180)
     F = zeros((len(x)), dtype=float64)
-    F[0] = C / (1 + e * sin(alpha)) - x[0]
-    F[1] = C / (1 + e * sin(alpha + pi/2)) - x[0]
-    F[2] = C / (1 + e * sin(alpha)) - C / (1 + e * sin(alpha + pi/2))
+    for i in range(len(R)):
+        F[i] = R[i] - x[0] / (1 + x[1] * sin(theta[i] + x[2]))
+    
     return F
 
 # Initial guess
-x = np.array([6800, 0.5, 0])  # What list needs to be used to initialze the Newton-Raphson (N-R) method?
+x = np.array([6800, 0.5, 0])
 
 # Complete the call to the N-R method to solve for unknowns
 x = newtonRaphson2(F, x)
@@ -66,8 +37,8 @@ np.set_printoptions(precision = 3)
 print('[ C  e  alpha] = ' + np.array_str(x))
 
 # Calculate minimum trajectory and angle using components of x
-minTheta = pi/2 - x[2]
-minR = x[0] / (1+ x[1] * sin(x[2]))
+minTheta = (pi / 2 - x[2]) * (180 / pi) # pi/2 - alpha in degrees
+minR = x[0] / (1 + x[1] * sin((minTheta * (pi/180)) + x[2]))
 
 # Print minimum trajectory results
 print('Minimum trajectory = %.3f km' % minR)
@@ -84,7 +55,7 @@ print()
 # Create arrays of points spaced every 0.01 radians around the satellite orbit
 # (theta) and their respective trajectories (R)
 
-theta = np.arange(0, 2*pi, 0.01)            # theta and R are arrays now
+theta = np.arange(0, 2*pi, 0.01) # theta and R are arrays now
 R = x[0] / (1 + x[1]*np.sin(theta + x[2]))
 
 # Plot orbit and minimum trajectory point
