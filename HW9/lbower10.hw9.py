df7ec6b2740e7e10618adba629d05001e7e30b88
Logan Bowers <84914281+lbower10@users.noreply.github.com>
2023-08-05 15:56:38 -0400

    HW9 done

--- HW9/lbower10.hw9.py
+++ HW9/lbower10.hw9.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-#
-# Template script for Problem 14 from Problemt Set 6.1 (p. 214 of textbook)
-#  
-# I this comment block, briefly explain the procedure referred to as
-# Romberg Integration and provide your name and assignment number.
-#  
-# ??? 
-# ???
-# ??? 
-#  
+'''
+  Logan Bowers - lbower10
+  COSC370 HW9
+  Romberg Integration is a numerical method used to
+  estimate a definite integral. It is a combination of
+  the Trapezoidal Rule and Richardson Extrapolation.
+  Here, we use this method to evaluate the indefinite integral
+  from 0 to 1/u of x^4 * e^x / (e^x - 1)^2, multiplied by u^3
+'''
+
 from romberg import *
 from numpy import *
 import matplotlib.pyplot as plt
 
 def f(x):
   if x == 0: return 0
-  else: return exp(-x ** 2) # define the integrable function here
+  else: return (x**4 * exp(x)) / (exp(x) - 1)**2
 
 u = arange(0,1.01,0.05)
 print ("    u\t   g(u)")
@@ -24,20 +24,26 @@ gu = [] #list that will contain all of g(u)s (y-coordinates for your plot)
 for i in u:
   if i == 0: g = 0.0
   else:
-    # perform romberg integration on f here (get result in
-    # I, nPanels is the number of panels used but is not
-    # used for output.
-    I,nPanels = romberg(f, 0, i)
-    g = I          # evaluate g(i) here
+    # perform romberg integration on f here
+    I,nPanels = romberg(f, 0, 1/i)
+    g = i**3 * I # evaluate g(i) here
   print ('{:6.2f}{:13.6f}'.format(i,g))
   gu.append(g)
 
 # Place the code that creates the required plot using pylab here.
 # Be sure to label axes and provide the same title as shown
 # in the "prob6_1-14.png" image file on BB
-#
+plt.plot(u, gu, 'blue')
+plt.xlim(0, 1.0)
+plt.ylim(0, 0.35)
+plt.xlabel('u')
+plt.ylabel('g(u)')
+plt.title('Problem 6.1.14')
+plt.tick_params(axis='both', which='both', direction='in', top=True, right=True)
+plt.savefig('prob6_1_14.png')
+plt.show()
+
 # Table written to stdout for verification purposes:
-#
 #  u	     g(u)
 # 0.00     0.000000
 # 0.05     0.003247
